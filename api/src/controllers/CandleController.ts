127dc2f5081500ccac84ca9ee3b73091d4ce09af
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-09-09 20:03:27 -0300

    reorganizando pastas

---
+++ api/src/controllers/CandleController.ts
@@ -0,0 +1,15 @@
+import { Candle, CandleModel } from "../models/CandleModel";
+
+export default class CandleController {
+
+    async save(candle: Candle): Promise<Candle> {
+        return await CandleModel.create(candle)
+    }
+    async findLastCandles(quantity: number): Promise<Candle[]> {
+        const n = quantity > 0 ? quantity : 10;
+        const lastCandles: Candle[] =
+            await CandleModel.find().sort({ _id: -1 }).limit(n)
+
+        return lastCandles;
+    }
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,17 @@ import { app } from "./app"
 import { config } from "dotenv"
 import { connectToMongoDB } from "./config/db"
 import { connection } from "mongoose"
+import CandleMessageChannel from "./messages/CandleMessageChannel"
 const createServer = async () => {
 
     config()
     await connectToMongoDB()
     const PORT = process.env.PORT
     const server = app.listen(PORT, () => console.log(`App running on port ${PORT}`))
+
+    const candleMsgChannel = new CandleMessageChannel(server)
+    candleMsgChannel.consumeMessages()
+
     process.on('SIGINT', async () => {
         await connection.close()
         server.close()
